ebd57c3d86aa29f9abeb83675295d8111b1f38ac
baayerische <yuralth200609@gmail.com>
2024-12-23 23:31:19 +0500

    Add files via upload

---
+++ Sem_1/2_17/2_17.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n;
+	float x, fact, sum;
+	cout << "enter n,x:" << endl;
+	cin >> n >> x;
+	sum = 1 + x;
+	fact = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		fact *= i;
+		sum += (pow(x, i) / fact);
+	}
+	cout << sum << endl;
+	return 0;
+}
