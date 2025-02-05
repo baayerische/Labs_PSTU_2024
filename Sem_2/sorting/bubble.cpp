b587aed63d6b971d3378be6661ce910648dbf7a0
baayerische <yuralth200609@gmail.com>
2025-02-05 22:38:06 +0500

    Update bubble.cpp

--- Sem_2/sorting/bubble.cpp
+++ Sem_2/sorting/bubble.cpp
@@ -1,32 +1,53 @@
-#include <iostream>
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 int main()
 {
-	int tmp;
-	const int n = 7;
-
-	int arr[n] = { 5, 2, 4, 6, 0, 1, 3 };
-		for (int i = 0; i < n; i++)
-		{
-			cout << arr[i] << " ";
-		}
-	cout << endl;
-
-	for (int i = 0; i < n - 1; i++)
-	{
-		for (int j = 0; j < n - 1 - i; j++)
-		{
-			if (arr[j] > arr[j + 1])
-			{
-				tmp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = tmp;
-			}
-		}
-	}
+	int r, range_min, range_max, tmp;
+	srand(time(0));
+	const int n = 3;
+	
+	int arr[n][n];
+	range_min = 2;
+	range_max = 280000;
+	
 	for (int i = 0; i < n; i++)
 	{
-		cout << arr[i] << " ";
+		for (int j = 0; j < n; j++)
+		{
+			arr[i][j] = ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			for (int k = n - 2; k >= 0; k--)
+			{
+				if (arr[j][k] > arr[j][k + 1])
+				{
+					tmp = arr[j][k];
+					arr[j][k] = arr[j][k + 1];
+					arr[j][k + 1] = tmp;
+				}
+			}
+
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
 	}
 	cout << endl;
 	return 0;
